b61a4511460d3b6859120eedb6f96d993292dd5f
Eugenio Mendoza <eugenio@wesend.co>
2019-10-09 16:07:56 -0500

    First commit

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+import { Common } from './zendesk-monety.common';
+export declare class ZendeskMonety extends Common {
+  // define your typings manually
+  // or..
+  // take the ios or android .d.ts files and copy/paste them here
+}
---
+++ src/zendesk-monety.ios.ts
@@ -0,0 +1,5 @@
+import { Common } from './zendesk-monety.common';
+
+export class ZendeskMonety extends Common {
+
+}
